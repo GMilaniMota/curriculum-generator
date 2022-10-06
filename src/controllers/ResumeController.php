e6cc05c689a2ceb8bf963bd418c25fe7806978d0
VinciPy <viniciusantana323@gmail.com>
2022-10-05 23:18:08 -0300

    feat: puload image

--- src/controllers/ResumeController.php
+++ src/controllers/ResumeController.php
@@ -6,6 +6,10 @@ use Entities\Resume;
 use Views\ResumeView;
 
 $data = $_POST;
+if (!move_uploaded_file($_FILES["picture"]["tmp_name"], '../../public/uploads'.$_FILES["picture"]["name"])) {
+    return false;
+}
+$data["picture"] = $_FILES["picture"]["name"];
 $resume = new Resume($data);
 $view  = new ResumeView($resume);
 $view->inflate();
